83a5b9747c313ec51e2c8f433aeee82b82358d8f
Denis <denis@sapps.(none)>
2012-06-16 13:59:33 +0400

    foreign_key

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,5 @@ class Patient < ActiveRecord::Base
   attr_accessible :address, :age, :card, :hasscan, :ltreatment_id, :name
 
   has_many :treatments
+  belongs_to :treatments, :foreign_key => "ltreatment_id"
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,6 +2,7 @@ class Treatment < ActiveRecord::Base
   attr_accessible :anamnesis, :complaints, :diagnosis, :diagnosis_id, :doctor_id, :patient_id, :plandate, :tests
 
   has_many :scans
+  has_one :patients, :foreign_key => "ltreatment_id"
   belongs_to :doctors
   belongs_to :patients
   belongs_to :diagnoses
