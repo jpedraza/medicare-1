efdf9fd741047dd9b2e5ddda0ad1af32e8fa69eb
Denis <denis@sapps.(none)>
2012-06-16 19:13:36 +0400

    Patien table format

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,5 +2,8 @@ class Patient < ActiveRecord::Base
   attr_accessible :address, :age, :card, :hasscan, :ltreatment_id, :name
 
   has_many :treatments
-  belongs_to :treatments, :foreign_key => "ltreatment_id"
+  has_one :lasttreatment, :class_name => "Treatment", :foreign_key => "id", :primary_key => "ltreatment_id", :include => [:doctor, :diagnosis]
+
+  #has_one :lastdoctor, :class_name => "Doctor", :through => :lasttreatment
+  #belongs_to :ltreatments, class_name => "Treatment", :foreign_key => "ltreatment_id"
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,7 +2,10 @@ class Treatment < ActiveRecord::Base
   attr_accessible :anamnesis, :complaints, :diagnosis, :diagnosis_id, :doctor_id, :patient_id, :plandate, :tests
 
   has_many :scans
-  has_one :patients, :foreign_key => "ltreatment_id"
+  #has_one :patients, :foreign_key => "ltreatment_id"
+  has_one :doctor, :class_name => "Doctor",  :foreign_key => "id", :primary_key => "doctor_id"
+  has_one :diagnosis, :class_name => "Diagnosis",  :foreign_key => "id", :primary_key => "diagnosis_id"
+
   belongs_to :doctors
   belongs_to :patients
   belongs_to :diagnoses
