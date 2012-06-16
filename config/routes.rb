3683b2e6fa77fe93b4c6c490463a28835132e863
Denis <denis@sapps.(none)>
2012-06-16 11:35:46 +0400

    Set root page

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Hero::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'patients#index'
 
   # See how all your routes lay out with "rake routes"
 
