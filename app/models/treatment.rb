34904ea68adfa0e31bf531d2a0a9c4afa46b1a41
Denny Brain <denibrain@gmail.com>
2012-06-17 10:58:37 +0400

    Treatment show

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,7 +2,8 @@ class Treatment < ActiveRecord::Base
   attr_accessible :anamnesis, :complaints, :diagnosis, :diagnosis_id, :doctor_id, :patient_id, :plandate, :tests
 
   has_many :scans
-  #has_one :patients, :foreign_key => "ltreatment_id"
+  
+  has_one :patient, :class_name => "Patient",  :foreign_key => "id", :primary_key => "patient_id"
   has_one :doctor, :class_name => "Doctor",  :foreign_key => "id", :primary_key => "doctor_id"
   has_one :diagnosis, :class_name => "Diagnosis",  :foreign_key => "id", :primary_key => "diagnosis_id"
 
