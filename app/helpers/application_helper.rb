36b7694dac68736f3c14924c437028d4cfaebff0
Denny Brain <denibrain@gmail.com>
2012-07-25 01:05:47 +0400

    Search, Sort & pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    column = column.to_s
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    flag = (column == sort_column) ? content_tag(:i, '', :class => (direction == 'asc' ? 'icon-chevron-up' : 'icon-chevron-down')) : ''
+    link_to(title, {:sort => column, :direction => direction, :page => params[:page], :search => params[:search]}) + ' ' + flag
+    
+  end	 
 end
--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -1,6 +1,14 @@
 class Diagnosis < ActiveRecord::Base
-  attr_accessible :code, :name
-  validates_presence_of :code, :name
+	attr_accessible :code, :name
+	validates_presence_of :code, :name
 
-  has_many :treatments
+	has_many :treatments
+
+	def self.search(search)
+	  if search
+	    where (['concat(name, \'--\', code) LIKE ?', "%#{search}%"])
+	  else
+	    scoped
+	  end
+	end  
 end
