f1288cc8ae04f23c5832a0c7d57ae0207264e2e0
Denny Brain <denibrain@gmail.com>
2012-08-06 00:30:41 +0400

    Base data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120721214409) do
+ActiveRecord::Schema.define(:version => 20120805184902) do
 
   create_table "diagnoses", :force => true do |t|
     t.string   "name"
